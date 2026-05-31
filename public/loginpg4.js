815ed519251b96de74c5f0e74cd03e2d81466b93
Subhajit Mandal <sm2532878@gmail.com>
2026-05-31 11:54:54 +0530

    Update loginpg4.js

--- public/loginpg4.js
+++ public/loginpg4.js
@@ -4,38 +4,16 @@ function registerUser() {
 
     fetch("/signup", {
         method: "POST",
-        headers: {"Content-Type": "application/json"},
+        headers: {
+            "Content-Type": "application/json"
+        },
         body: JSON.stringify({ name, email })
     })
     .then(res => res.json())
-    .then(data => document.getElementById("msg").innerText = data.message);
+    .then(data => {
+        document.getElementById("msg").innerText = data.message;
+    });
 }
-
-// function sendOTP() {
-//     const email = document.getElementById("loginEmail").value;
-
-//     fetch("/send-otp", {
-//         method: "POST",
-//         headers: {"Content-Type": "application/json"},
-//         body: JSON.stringify({ email })
-//     })
-//     .then(res => res.json())
-//     .then(data => document.getElementById("msg").innerText = data.message);
-// }
-
-// function verifyOTP() {
-//     const email = document.getElementById("loginEmail").value;
-//     const otp = document.getElementById("loginOTP").value;
-
-//     fetch("/verify-otp", {
-//         method: "POST",
-//         headers: {"Content-Type": "application/json"},
-//         body: JSON.stringify({ email, otp })
-//     })
-//     .then(res => res.json())
-//     .then(data => {
-// if (data.success){window.location.href="/landing"} else {document.getElementById("msg").innerText = data.message}});
-// }
 
 function sendOTP() {
     const email = document.getElementById("loginEmail").value;
@@ -49,36 +27,59 @@ function sendOTP() {
     })
     .then(res => res.json())
     .then(data => {
-
         document.getElementById("msg").innerText = data.message;
 
-        if(data.success){
+        if (data.success) {
             alert("Your OTP is: " + data.otp);
         }
-        .then(res => res.json())
-     .then(data => {
- if (data.success){window.location.href="/landing"} else {document.getElementById("msg").innerText = data.message}});
+    })
+    .catch(err => {
+        console.error(err);
     });
 }
-function adminLogin(){
+
+function verifyOTP() {
+    const email = document.getElementById("loginEmail").value;
+    const otp = document.getElementById("loginOTP").value;
+
+    fetch("/verify-otp", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ email, otp })
+    })
+    .then(res => res.json())
+    .then(data => {
+        if (data.success) {
+            window.location.href = "/landing";
+        } else {
+            document.getElementById("msg").innerText = data.message;
+        }
+    });
+}
+
+function adminLogin() {
     const user = document.getElementById("user").value;
     const pass = document.getElementById("pass").value;
+
     fetch("/admin/login", {
         method: "POST",
-        headers: {"Content-Type": "application/json"},
+        headers: {
+            "Content-Type": "application/json"
+        },
         body: JSON.stringify({ user, pass })
     })
     .then(res => res.json())
     .then(data => {
-if (data.success){window.location.href="/admindashboard"} else {document.getElementById("msg").innerText = data.message}});
-
+        if (data.success) {
+            window.location.href = "/admindashboard";
+        } else {
+            document.getElementById("msg").innerText = data.message;
+        }
+    });
 }
-function submitNews(){
- fetch("/news",{method:"GET"}).then(data=>{if(data.success){window.location.href="./news"}}) 
+
+function submitNews() {
+    window.location.href = "/news.html";
 }
-
-
-
-
-
-
